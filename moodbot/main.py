d0854a94b849516de095cd3fc185f8286d48c997
ricwo <ric.wkr@gmail.com>
2018-05-22 17:43:05 +0100

    retrained nlu and core

--- moodbot/main.py
+++ moodbot/main.py
@@ -1,8 +1,10 @@
-from rasa_core.channels.rest import HttpInputChannel
-from rasa_extensions.core.remote import run_with_remote_core
-from rasa_core.domain import TemplateDomain
-import os
 import logging
+import os
+
+from rasa_core.channels.rest import HttpInputChannel
+from rasa_core.domain import TemplateDomain
+from rasa_core.remote import RasaCoreClient, RemoteAgent
+from rasa_extensions.core.remote import run_with_remote_core
 
 if __name__ == "__main__":
     logging.basicConfig(level="DEBUG")
@@ -13,12 +15,12 @@ if __name__ == "__main__":
     # instantiate the input channel you want to connect to
     from rasa_extensions.core.channels.rasa_chat import RasaChatInput
 
-    input_channel = HttpInputChannel(5001,
-                                     "/",
-                                     RasaChatInput(os.environ.get("RASA_API_ENDPOINT_URL"))
-                                     )
+    input_channel = HttpInputChannel(
+        5001, "/", RasaChatInput(os.environ.get("RASA_API_ENDPOINT_URL")))
 
-    run_with_remote_core(os.environ.get("RASA_REMOTE_CORE_ENDPOINT_URL"),
-                         os.environ.get("RASA_CORE_TOKEN"),
-                         domain,
-                         input_channel)
+    client = RasaCoreClient(os.environ.get("RASA_REMOTE_CORE_ENDPOINT_URL"),
+                            os.environ.get("RASA_CORE_TOKEN"))
+
+    agent = RemoteAgent(domain, client)
+
+    agent.handle_channel(input_channel)
