00261f80326e8282375061e0e84556a373c19032
ricwo <ric.wkr@gmail.com>
2020-02-28 08:40:51 +0100

    update to rasa==1.8.0 in requirements.txt

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
 
     # if you create custom actions, use a module path for the action package to
     # point the server to them e.g. "mymodule.actions"
+    # noinspection PyTypeChecker
     endpoint.run(
         None,  # action package
         constants.DEFAULT_SERVER_PORT,  # port of the web server
